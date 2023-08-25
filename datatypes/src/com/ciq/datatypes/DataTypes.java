f87fd04d35966c3ee2d08369ef1d9a236ddb6e16
chand <chand@LAPTOP-6L02EGEP>
2023-08-25 11:00:37 +0530

    Data types

---
+++ datatypes/src/com/ciq/datatypes/DataTypes.java
@@ -0,0 +1,32 @@
+package com.ciq.datatypes;
+
+public class DataTypes {
+public static void main(String [] args) {
+	boolean isEligible=true;
+	char gender='m';
+	float value=20.5f;
+	System.out.println("it is a boolean value:" +isEligible);
+	System.out.println("it is a char value:" + gender);
+	System.out.println("it is a float value:" + value);
+	
+	
+	
+	// integers
+	byte number=-128;
+	System.out.println("it is a byte number" + number);
+	short shortValue= -32768;
+	System.out.println("it is a shortValue"+ shortValue);
+	int Value=268547;
+	int phoneNumber=84_98_56598;
+	System.out.println("it is a integer value:"+Value);
+	System.out.println("it is phone number:"+ phoneNumber);
+	
+	
+	
+	
+			
+	
+			
+			
+}
+}
